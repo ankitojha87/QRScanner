5393270a79c8d5d644231df2c1b4478a20a51a35
Ankit Ojha <ankkitojha@live.com>
2020-06-15 17:39:05 +0530

    QR Scanner code for the test

---
+++ QRScanner/Controllers/QRCodeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QRScanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QRCodeController : ControllerBase
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<IActionResult> Scan()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    using (HttpClient client = new HttpClient())
+                    {
+
+                        var baseUrl = "http(s)://api.qrserver.com/v1/read-qr-code/?fileurl=" + dbPath;
+                        using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+                        {
+                            using (HttpContent content = res.Content)
+                            {
+                                string codeText = await content.ReadAsStringAsync();
+                                if (codeText != null)
+                                {
+                                    return Ok(codeText);
+                                }
+                            }
+                        }
+                    }
+
+                    
+
+                    return Ok(null);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
